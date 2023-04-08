e7181b816adbe404b3a1d6583061aadf83deb7db
itx-shoaib <shoaibjamil43@gmail.com>
2023-04-09 02:51:14 +0500

    making userModels.js

---
+++ Models/userModel.js
@@ -0,0 +1,56 @@
+import mongoose from "mongoose"
+
+const userSchema = mongoose.Schema({
+    username: {
+        type: String,
+        unique: true,
+        required: [true, "Please enter the username"]
+    },
+    email: {
+        type: String,
+        required: [true, "Please enter a email"],
+        trim: true,
+        unique: true,
+        match: [
+            /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
+            "Please enter a valid email"
+        ]
+    },
+    password: {
+        type: String,
+        required: [true, "Please enter a password"],
+        minLength: [6, "Password must be greater then 6 digits"],
+        maxLength: [23, "Password must be less then 23 digits"]
+    },
+    firstname: {
+        type: String,
+        required: [true, "Please enter firstname"]
+    },
+    lastname: {
+        type: String,
+        required: [true, "Please enter the lastname"]
+    },
+    isAdmin: {
+        type: Boolean,
+        default: false
+    },
+    profilePicture: String,
+    coverPicture: String,
+    about: String,
+    livesin: String,
+    worksAt: String,
+    relationship: String,
+    followers: [{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'users'
+    }],
+    following: [{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'users'
+    }]
+},
+    { timestamps: true })
+
+const userModal = mongoose.model('users', userSchema)
+
+export default userModal
