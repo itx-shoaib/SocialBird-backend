7d5e059064db5cce79d0cf2e79ef389ab88bbc4f
itx-shoaib <shoaibjamil43@gmail.com>
2023-04-09 19:21:36 +0500

    Making login api

--- Controllers/AuthController.js
+++ Controllers/AuthController.js
@@ -30,6 +30,47 @@ export const registerUser = async (req, res) => {
     } catch (error) {
         // Handle errors
         console.error(error)
-        res.status(500).json({ error: "Something went wrong" })
+        res.status(500).json({ error: error.message })
+    }
+}
+
+export const loginUser = async (req, res) => {
+    try {
+        const { username, password } = req.body;
+
+        // Validate user input
+        if (!username || !password) {
+            return res.status(400).json({ error: "Please provide username and password" });
+        }
+
+        // Check if user exists
+        const user = await userModal.findOne({ username });
+        if (!user) {
+            return res.status(404).json({ error: "User not found" });
+        }
+
+        // Check if password is correct
+        const passwordMatch = await bcrypt.compare(password, user.password);
+        if (!passwordMatch) {
+            return res.status(400).json({ error: "Invalid password" });
+        }
+
+        // Generate JWT token
+        const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET);
+
+        // Set the token as a cookie
+        res.cookie("token", token, {
+            httpOnly: true,
+            secure: true,
+            sameSite: "none",
+            maxAge: 24 * 60 * 60 * 1000, // 1 day
+        });
+
+        // Return success response with JWT token
+        res.status(200).json({ message: "Login successful" });
+    } catch (error) {
+        // Handle errors
+        console.error(error);
+        res.status(500).json({ error: error.message });
     }
 }
--- Routes/AuthRoute.js
+++ Routes/AuthRoute.js
@@ -1,12 +1,12 @@
 import express from "express"; //import express
-import { registerUser } from "../Controllers/AuthController.js";
+import { loginUser, registerUser } from "../Controllers/AuthController.js";
 
 // 1.
 const router = express.Router();
 
 // 2.
 router.post('/register', registerUser);
-// router.post('/loginUser', UserController.loginUser);
+router.post('/login', loginUser)
 
 // 3. 
 export default router; // export to use in index.js
