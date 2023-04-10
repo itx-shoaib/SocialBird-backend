0325540f57732f7376d2c8cc9f82317e48ae00e9
awais-shahbaz-tech <awaishahbaz@gmail.com>
2023-04-11 04:47:27 +0500

    updatepost api

--- Routes/PostRoute.js
+++ Routes/PostRoute.js
@@ -1,6 +1,6 @@
 import express from "express"; //import express
 import authMiddleware from "../middlewear/authMiddlewear.js"
-import { createPost, getPost } from "../Controllers/PostControllers.js";
+import { createPost, getPost, updatePost } from "../Controllers/PostControllers.js";
 
 
 // 1.
@@ -9,6 +9,7 @@ const router = express.Router();
 // 2.
 router.post("/createPost", authMiddleware, createPost)
 router.get("/getPost/:id", getPost)
+router.put("/updatePost", authMiddleware, updatePost)
 
 // 3. 
 export default router; // export to use in index.js
