6905e432f76d1669a05041e92ea0323c1577a219
itx-shoaib <shoaibjamil43@gmail.com>
2023-06-06 01:35:49 +0500

    Making UploadRoute and multer code

---
+++ Routes/UploadRoute.js
@@ -0,0 +1,14 @@
+import express from "express";
+const router = express.Router();
+import multer from "multer";
+
+const storage = multer.diskStorage({
+    destination:(req,file,cb)=>{
+        cb(null,"public/images");
+    },
+    filename:(req,file,cb)=>{
+        cb(null,req.body.name);
+    }
+})
+
+const upload = multer({storage:storage})
